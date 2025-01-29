cd4c3e645050c5de0514710bc31a93a6d125c18c
hardik <113549733+ketan2098@users.noreply.github.com>
2025-01-29 23:06:41 +0530

    minor change

--- src/components/three/ModelViewer/ModelViewer.tsx
+++ src/components/three/ModelViewer/ModelViewer.tsx
@@ -1,26 +1,27 @@
-import { Bounds, useGLTF } from '@react-three/drei'
-import { modelElementsAtom } from '@store/modelStore'
-import { useAtom } from 'jotai'
-import { useEffect } from 'react'
+import { Bounds, useGLTF } from "@react-three/drei";
+import { modelElementsAtom } from "@store/modelStore";
+import { useAtom } from "jotai";
+import { useEffect } from "react";
 
 const ModelViewer = () => {
+  const { scene, nodes } = useGLTF("/hoodie.glb");
+  // console.log(nodes)
 
-    const { scene, nodes } = useGLTF('/hoodie.glb')
-    console.log(nodes)
+  const [, setmodelElements] = useAtom(modelElementsAtom);
 
-    const [, setmodelElements] = useAtom(modelElementsAtom)
+  useEffect(() => {
+    const arrayOfObj = Object.entries(nodes).map((e) => ({ [e[0]]: e[1] }));
 
-    useEffect(() => {
-        const arrayOfObj = Object.entries(nodes).map((e) => ({ [e[0]]: e[1] }));
-        
-        setmodelElements(arrayOfObj)
-    }, [])
+    setmodelElements(arrayOfObj);
+  }, []);
 
-    return (
-        scene && <Bounds>
-            <primitive object={scene} />
-        </Bounds>
+  return (
+    scene && (
+      <Bounds>
+        <primitive object={scene} />
+      </Bounds>
     )
-}
+  );
+};
 
-export default ModelViewer
+export default ModelViewer;
--- src/store/modelStore.ts
+++ src/store/modelStore.ts
@@ -1,4 +1,8 @@
 import { atom } from "jotai";
+import { Object3D, Object3DEventMap } from "three";
 
-export const modelUrlAtom = atom('')
-export const modelElementsAtom = atom([])
+type ModelElement = { [key: string]: Object3D<Object3DEventMap> };
+
+export const modelUrlAtom = atom("");
+// export const modelElementsAtom = atom([])
+export const modelElementsAtom = atom<ModelElement[]>([]);
