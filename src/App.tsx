f67870c21cc501c0d090ff37b5efcee3a5c25bca
hardik <113549733+ketan2098@users.noreply.github.com>
2025-01-29 23:12:59 +0530

    complate one or two point

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,30 @@
-import './App.scss'
-import { Divider, HStack, VStack } from '@chakra-ui/react';
-import { Settings } from './components/UI/settings/Settings';
-import Canvas3D from './components/three/canvas3D/Canvas3D';
-import Elements from './components/UI/Elements/Elements';
-import ColorPickerContainer from './components/UI/ColorPicker/ColorPickerContainer';
-import SideBarHeader from './components/UI/SideBarHeader/SideBarHeader';
-import BreadCrumps from './components/UI/BreadCrumps/BreadCrumps';
-import FileViewer from './components/UI/FileViewer/FileViewer';
-import FileViewerSingle from './components/UI/FileViewer/FileViewerSingle';
-import TextureLibrary from './components/UI/TextureLibrary/TextureLibrary';
-import UploadForm from './components/UI/UploadForm/UploadForm';
+import "./App.scss";
+import { Divider, HStack, VStack } from "@chakra-ui/react";
+import { Settings } from "./components/UI/settings/Settings";
+import Canvas3D from "./components/three/canvas3D/Canvas3D";
+import Elements from "./components/UI/Elements/Elements";
+import ColorPickerContainer from "./components/UI/ColorPicker/ColorPickerContainer";
+import SideBarHeader from "./components/UI/SideBarHeader/SideBarHeader";
+import BreadCrumps from "./components/UI/BreadCrumps/BreadCrumps";
+import FileViewer from "./components/UI/FileViewer/FileViewer";
+import FileViewerSingle from "./components/UI/FileViewer/FileViewerSingle";
+import TextureLibrary from "./components/UI/TextureLibrary/TextureLibrary";
+import UploadForm from "./components/UI/UploadForm/UploadForm";
 
 function App() {
-
   return (
     <>
-      <HStack
-        height={'100vh'}
-      >
+      <HStack height={"100vh"}>
         <VStack
-          height={'100vh'}
-          maxHeight={'100vh'}
-          width={'450px'}
-          bgColor={'bgDGray'}
-
-        >
-          <SideBarHeader />
-          <VStack
-            width={'full'}
-            overflowY={'auto'}
-          >
+          height={"100vh"}
+          maxHeight={"100vh"}
+          width={"450px"}
+          bgColor={"bgDGray"}>
+          {/* <SideBarHeader /> */}
+          <VStack width={"full"} overflowY={"auto"}>
             <BreadCrumps />
             <FileViewer />
-            <FileViewerSingle />
+            {/* <FileViewerSingle /> */}
             <TextureLibrary />
             <UploadForm />
             <Divider />
@@ -44,7 +36,7 @@ function App() {
         <Canvas3D />
       </HStack>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
